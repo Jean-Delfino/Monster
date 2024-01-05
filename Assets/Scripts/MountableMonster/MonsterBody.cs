3716e526a38685baa5668a54cf64e89f2e1fd142
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-05 18:04:14 -0300

    Monster parts
    I thought that it was already in previous commits.

--- Assets/Scripts/MountableMonster/MonsterBody.cs
+++ Assets/Scripts/MountableMonster/MonsterBody.cs
@@ -1,7 +1,14 @@
-﻿namespace MountableMonster
+﻿using UnityEngine;
+
+namespace MountableMonster
 {
-    public class MonsterBody
+    public class MonsterBody : MonoBehaviour
     {
-        
+        [SerializeField] private PartConnection connection;
+
+        public void SetMainConnection(MonsterPart monsterPart)
+        {
+            connection.ConnectPart(monsterPart);
+        }
     }
 }
--- Assets/Scripts/MountableMonster/MonsterPart.cs
+++ Assets/Scripts/MountableMonster/MonsterPart.cs
@@ -1,7 +1,32 @@
-﻿namespace MountableMonster
+﻿using UnityEngine;
+
+namespace MountableMonster
 {
-    public class MonsterPart
+    public class MonsterPart : MonoBehaviour
     {
+        [SerializeField] private PartConnection[] connections;
+
+        private bool _isConnected = false;
+        public void ConnectMonsterPartToConnection(int connectionIndex, MonsterPart monsterPart)
+        {
+            ConnectMonsterPartToConnection(connections[connectionIndex], monsterPart);
+        }
+
+        public void ConnectMonsterPartToConnection(PartConnection connection, MonsterPart monsterPart)
+        {
+            connection.ConnectPart(monsterPart);
+            
+            transform.position = connection.transform.position;
+        }
         
+        public void SetConnected(bool state)
+        {
+            _isConnected = state;
+        }
+
+        public void SetRotation(Vector3 rotation)
+        {
+            transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
