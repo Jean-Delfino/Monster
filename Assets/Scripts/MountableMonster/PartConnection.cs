ad4e4c8338584b10acd39bb51b7f305f8b0be70e
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-05 18:03:42 -0300

    Part connection.

--- Assets/Scripts/MountableMonster/PartConnection.cs
+++ Assets/Scripts/MountableMonster/PartConnection.cs
@@ -1,7 +1,17 @@
-﻿namespace MountableMonster
+﻿using UnityEngine;
+
+namespace MountableMonster
 {
-    public class PartConnection
+    public class PartConnection : MonoBehaviour
     {
-        
+        [SerializeField] private Vector3 partRotation = Vector3.zero;
+
+        private MonsterPart _connectedPart;
+
+        public void ConnectPart(MonsterPart part)
+        {
+            part.SetConnected(true);
+            part.SetRotation(partRotation);
+        }
     }
 }
