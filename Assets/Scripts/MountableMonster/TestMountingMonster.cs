1b9c113b1643b37dfc47ba3ba04e48cb84f2c2b5
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-05 18:03:26 -0300

    Script for testing and fixing the parts spawn.

---
+++ Assets/Scripts/MountableMonster/TestMountingMonster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace MountableMonster
+{
+    public class TestMountingMonster : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        //Prefabs
+        [SerializeField] private MonsterBody monsterBody;
+        
+        [Serializable]
+        public class SpawnPart
+        {
+            public MonsterPart part;
+            public int connectToPartId;
+            public int connectToConnectionId;
+        }
+
+        [SerializeField] private SpawnPart[] spawnParts;
+        private void Start()
+        {
+            CombineMonster();
+        }
+
+        private void CombineMonster()
+        {
+            var monsterBodyClone = Instantiate(monsterBody) as MonsterBody;
+
+            MonsterPart[] parts = new MonsterPart[spawnParts.Length];
+
+            for (int i = 0; i < spawnParts.Length; i++)
+            {
+                parts[i] = Instantiate(spawnParts[i].part, monsterBodyClone.transform) as MonsterPart;
+            }
+
+            monsterBody.SetMainConnection(parts[0]);
+
+            for (int i = 1; i < parts.Length; i++)
+            {
+                var connectToPart = parts[spawnParts[i].connectToPartId];
+                
+                connectToPart.ConnectMonsterPartToConnection(spawnParts[i].connectToConnectionId, parts[i]);
+            }
+        }
+        
+#endif
+    }
+}
