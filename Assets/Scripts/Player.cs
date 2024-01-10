40e73bee1776ea6ea2692a74d2b2edb2895a6b7e
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-10 17:34:25 -0300

    Making the nav mesh code
    - Offset is still not perfect, why, sincerely I don't know, maybe some math problem with the bounds, or something I'm not fully aware.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,39 @@
 ﻿//This player is the "mountable monster" or it's controller, so, yes, he'll probably be a weird player, but several thing will be made thrown 
 
-public class Player
+using System;
+using Actors;
+using MountableMonster;
+using UnityEngine;
+
+public class Player : MonoBehaviour
 {
+    [SerializeField] private Actor playerActor;
+
+    private MonsterBody _playerMonsterBody;
+
+#if UNITY_EDITOR
+    //Just for any given test
+    public void SetMonsterBody(MonsterBody monsterBody)
+    {
+        this._playerMonsterBody = monsterBody;
+    }
+#endif
+    
+    public void PrepareNavMesh()
+    {
+        var bounds = _playerMonsterBody.FindBodyExtends();
         
+        var agent = playerActor.Agent;
+        var agentTransform = agent.transform;
+        var agentPos = agentTransform.position;
+        
+        agent.height = bounds.MaxY - bounds.MinY;
+        agent.radius = (bounds.MaxX - bounds.MinX) / 2;
+        
+        var newX = (float)Math.Round((bounds.MinX + bounds.MaxX) / 2, 4); //Precision of "only" 4 
+        
+        agent.baseOffset = Mathf.Abs(bounds.MinY); 
+        _playerMonsterBody.Connection.transform.localPosition = new Vector3(-newX, 0, 0);
+    }
+    
 }
