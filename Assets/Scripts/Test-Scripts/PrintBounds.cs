d0976a1cf5ee3197989f86eb1a37c1a8cfe5af84
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-10 15:24:07 -0300

    More auxiliary classes for the game testing.

---
+++ Assets/Scripts/Test-Scripts/PrintBounds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Test_Scripts
+{
+    public class PrintBounds : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Renderer childRenderer = GetComponent<Renderer>();
+            var bounds = childRenderer.bounds;
+            print("BOUNDS(MAX X, MAX Y, MIN X, MIN Y) :" + bounds.max.x + " " + bounds.max.y + " " + bounds.min.x + " " + bounds.min.y);
+            print("CENTER = " + bounds.center);
+        }
+    }
+}
